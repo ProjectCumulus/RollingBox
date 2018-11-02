39c258e23a71106282d1a86e63830e595f676cf3
5681jin <5681jin@naver.com>
2018-11-02 14:03:04 +0900

    임시타이머추가

---
+++ 굴러라! BOX/Assets/Script/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text TimerText;
+    public string additionalText = string.Empty;
+    // Use this for initialization
+    void Start ()
+    {
+        DontDestroyOnLoad(this);
+        StartCoroutine(UpdateTime());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    IEnumerator UpdateTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+            TimerText.text = additionalText + Time.time;
+        }
+    }
+}
