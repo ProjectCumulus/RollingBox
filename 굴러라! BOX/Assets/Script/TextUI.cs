283cb4cd505a93318b254628824b0c3bf56f339e
5681jin <5681jin@naver.com>
2018-11-07 11:55:33 +0900

    튜토리얼 UI추가

---
+++ 굴러라! BOX/Assets/Script/TextUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextNUM
+{
+    public static int TriggerNum = 0;
+}
+
+
+public class TextUI : MonoBehaviour
+{
+    public Text TTT;
+    TextNUM TextNUM;
+    string[] Tutorial = new string[20];
+
+	// Use this for initialization
+	void Start ()
+    {
+        Tutorial[0] = "1번 메세지";
+        Tutorial[1] = "2번 메세지";
+        Tutorial[2] = "3번 메세지";
+        Tutorial[3] = "4번 메세지";
+        Tutorial[4] = "5번 메세지";
+        Tutorial[5] = "6번 메세지";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            TTT.text = Tutorial[TextNUM.TriggerNum];
+            TextNUM.TriggerNum++;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
