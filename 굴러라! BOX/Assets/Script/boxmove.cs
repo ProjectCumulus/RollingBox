a98ce7de26cb1510297ab62527335a07655a3c16
whe0107 <whe0107@naver.com>
2018-11-08 11:30:50 +0900

    크레딧 상자들 무한 루프 구현 완료

--- 굴러라! BOX/Assets/Script/boxmove.cs
+++ 굴러라! BOX/Assets/Script/boxmove.cs
@@ -15,7 +15,6 @@ public class boxmove : MonoBehaviour
 
     Rigidbody2D rb;
     public float MoveSpeed = 0.0125f;
-    Ending ending;
     private void Awake()
     {
 
@@ -26,7 +25,7 @@ public class boxmove : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         Time.timeScale = 1f;
-        ending = GameObject.Find("GoEnding").GetComponent<Ending>();
+        //ending = GameObject.Find("GoEnding").GetComponent<Ending>();
     }
 
 
@@ -43,6 +42,14 @@ public class boxmove : MonoBehaviour
             transform.position = new Vector2(-28, transform.position.y);
         }
         */
+        if (SceneManager.GetActiveScene().name == "Ending")
+        {
+            if (transform.position.x > 105)
+            {
+                transform.position = new Vector2(-25, transform.position.y);
+            }
+        }
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -53,73 +60,5 @@ public class boxmove : MonoBehaviour
     }
 }
 
-        /*
-        private void FixedUpdate()
-        {
-            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
-            {
-                if (JumpAble)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, JumpPower);
-                }
-            }
-
-            if (Input.GetKey(KeyCode.D))
-            {
-                if (KeyInputCheck == 0 && BoxRotation == 1)
-                {
-                    KeyInputCheck = 1;
-                    RightLeftDistinction = 1;
-                }
-            }
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                if (KeyInputCheck == 0 && BoxRotation == 1)
-                {
-                    KeyInputCheck = 1;
-                    RightLeftDistinction = -1;
-                }
-            }
-
-            if (KeyInputCheck == 1 && BoxRotation == 1)
-            {
-                KeyInputCheck = 0;
-                rb.freezeRotation = true;
-                InvokeRepeating("BoxMove", 0, 0.001f);
-            }
-
-        }
-        */
-        /*
-        void BoxMove()
-        {
-            transform.position += new Vector3(RightLeftDistinction * MoveSpeed , 0, 0);//이동
-            this.transform.Rotate(new Vector3(0, 0, RightLeftDistinction * -RotateAngle));//회전
-            BoxRotation++;
-            if(BoxRotation>50)
-            {
-                rb.freezeRotation = false;
-                CancelInvoke("BoxMove");
-                BoxRotation = 1;
-            }
-        }
-
-
-
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if(collision.gameObject.layer==8)
-            {
-                JumpAble = true;
-            }
-        }
-
-        private void OnCollisionExit2D(Collision2D collision)
-        {
-            if (collision.gameObject.layer == 8)
-            {
-                JumpAble = false;
-            }
-        }
-        */
+         
+    
