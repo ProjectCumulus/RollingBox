17f9da6c5ba0d35f61f68ffab641bac3c90a589e
Choi33 <rena258@naver.com>
2018-11-08 22:48:24 +0900

    엔딩 크레딧
    검은 투명창 만듦

--- 굴러라! BOX/Assets/Script/Credit.cs
+++ 굴러라! BOX/Assets/Script/Credit.cs
@@ -8,12 +8,14 @@ public class Credit : MonoBehaviour
     //public GameObject credit1, credit2;
 
     public Text Credit_Text;
+    public Image Na;
     public GameObject DataSave;
 
     // Use this for initialization
     void Start()
     {
         DataSave.gameObject.SetActive(false);
+        Na.gameObject.SetActive(false);
         StartCoroutine(CreditTextPrint());
 
     }
@@ -21,6 +23,7 @@ public class Credit : MonoBehaviour
     IEnumerator CreditTextPrint()
     {
         yield return new WaitForSeconds(2.0f);
+        Na.gameObject.SetActive(true);
         Credit_Text.text = "\n팀명\n▶뭉게구름";
         yield return new WaitForSeconds(3.5f);
         Credit_Text.text = "\n조원\n▶김진혁 - 전반적인 코드 작성\n▶서유란 - 디자인\n▶위하은 - 스프라이트 배치 및 조정\n▶최예슬 - 디자인";
@@ -29,7 +32,7 @@ public class Credit : MonoBehaviour
         yield return new WaitForSeconds(2.0f);
         Credit_Text.text = "\n오픈소스\n▶상자 이동 - https://github.com/cjddmut/Unity-2D-Platformer-Controller\n▶HP UI - tankandhealerstudio\n\n이미지\n▶방향키 - Icon made by Freepik from www.flaticon.com\n▶말풍선 - Created by Freepik";
         yield return new WaitForSeconds(3.5f);
-        Credit_Text.text = "\n사운드\n▶앱센터\nStage1, Laser, Book, Sword, Mine, Bombing, AIrplane, Move, Death, Button\n\n▶Kevin MacLeod (incompetech.com)\nMalicious(Stage4)\n▶Timbre(https://freesound.org/s/221683/)\nanother magic wand spell tinkle(Recovery)";
+        Credit_Text.text = "\n사운드\n ▶앱센터\nStage1, Laser, Book, Sword, Mine, Bombing, AIrplane, Move, Death, Button\n\n▶Kevin MacLeod (incompetech.com)\n Malicious(Stage4)\n▶Timbre(https://freesound.org/s/221683/)\nanother magic wand spell tinkle(Recovery)";
         yield return new WaitForSeconds(3.5f);
         Credit_Text.text = "\n사운드 프리소스\n▶Northen_LIghts(MainScreen)\n▶White_Hats(Stage2)\n▶Spirit of the Dead(Stage3)\n▶water throw stone1(Water)\n▶paper tear3(Damaged)";
         yield return new WaitForSeconds(3.0f);
