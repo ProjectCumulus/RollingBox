310b32de49aaea56648960594820a60b59c49fd7
5681jin <5681jin@naver.com>
2018-11-08 20:37:49 +0900

    튜토리얼 비데미지 수정

--- 굴러라! BOX/Assets/Script/RainTrigger.cs
+++ 굴러라! BOX/Assets/Script/RainTrigger.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class RainTrigger : MonoBehaviour {
     public GameObject Rain, Rain_S;
+    BoxHP BoxHP;
+    GameObject Player;
     // Use this for initialization
     void Start()
     {
+        Player = GameObject.Find("Player");
+        BoxHP=Player.GetComponent<BoxHP>();
         Rain.gameObject.SetActive(false);
         Rain_S.gameObject.SetActive(false);
     }
@@ -20,8 +24,10 @@ public class RainTrigger : MonoBehaviour {
     {
         if (coll.gameObject.tag == "Player")
         {
+            BoxHP.Rain();
             Rain.gameObject.SetActive(true);
             Rain_S.gameObject.SetActive(true);
+            //Destroy(this);
         }
     }
 }
