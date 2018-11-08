54a27ba0ea0fe36b18f174a4fc182622319408de
5681jin <5681jin@naver.com>
2018-11-08 15:59:21 +0900

    치트키추가

---
+++ 굴러라! BOX/Assets/Script/Cheat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheat : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.P))
+        {
+            transform.position = new Vector2(GameObject.Find("덤덤이").transform.position.x - 10, 5);
+           
+        }
+	}
+}
