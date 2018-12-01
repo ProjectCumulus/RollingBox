ad8d5d6f9eaccb750dc1e3d2e9847768b800667e
seoyl98 <seoyla98@gmail.com>
2018-12-01 19:32:07 +0900

    배드엔딩 최종

---
+++ 굴러라! BOX/Assets/Script/GameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+    Fade Black;
+    public Text Game_Over;
+
+    // Use this for initialization
+    void Start () {
+        Black = GameObject.Find("Black").GetComponent<Fade>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Game_Over.text = "Game Over";
+            Debug.Log("떴따");
+        }
+    }
+}
